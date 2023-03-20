c1950f83dd1adc95939ec57d9b6c1d004fb44743
TurpialCode <daniel.oreste@col.vueling.com>
2023-03-20 16:55:39 +0100

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 import { MainpipePipe } from './pipes/mainpipe.pipe';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MainpipePipe } from './pipes/mainpipe.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContenedorComponent } from './contenedor/contenedor.component';
+import { CartaComponent } from './carta/carta.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContenedorComponent,
+    CartaComponent
+  ],
+  exports:[  ContenedorComponent,
+    CartaComponent],
   imports: [
     CommonModule
   ]
